0fe7d38f63c4ebf69c5684099fdccb22a5d99f85
Zelin Wan <wanzelin007@gmail.com>
2019-01-20 06:54:49 -0700

    backup

---
+++ index.js
@@ -0,0 +1,103 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+const Alexa = require('alexa-sdk');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID = undefined;
+
+const SKILL_NAME = 'Faery Talk';
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
+    },
+    'GetNewFactIntent': function () {
+        const speechOutput = 'I am your fairy. I am a listener. And I hope I can always be there for you when you need me.'
+
+        this.response.cardRenderer(SKILL_NAME, 0);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+      'noLove': function () {
+        const speechOutput = "because they are not little angels like you. They don't understand you.";
+
+        this.response.cardRenderer(SKILL_NAME, 1);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+          'AmIBad': function () {
+        const speechOutput = "you are not bad, you are just special.";
+
+        this.response.cardRenderer(SKILL_NAME, 2);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+          'noBroccoli': function () {
+        const speechOutput = "You need to finish your broccoli so you can have dessert today";
+
+        this.response.cardRenderer(SKILL_NAME, 3);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+          'Terrible': function () {
+        const speechOutput = "you are not terrible, you are just special.";
+
+        this.response.cardRenderer(SKILL_NAME, 4);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+          'HowDoYouKnow': function () {
+        const speechOutput = "I'm your best friend. I know you very well";
+
+        this.response.cardRenderer(SKILL_NAME, 5);
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+//*************************************
+
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+ 
